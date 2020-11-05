7a30fd80385780d5fe9c373c70ef3d9966ca38ea
phantom <18771866669@139.com>
2020-11-05 15:29:27 +0800

    add v2

--- util/method.go
+++ util/method.go
@@ -3,9 +3,10 @@ package util
 import (
 	"reflect"
 
-	//builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
+
 )
 
 func LotusMethodToStr(code cid.Cid, methodNum uint64) string {
@@ -17,43 +18,43 @@ func LotusMethodToStr(code cid.Cid, methodNum uint64) string {
 		return "MethodConstructor"
 	}
 
-	if code == builtin2.AccountActorCodeID {
+	if code == builtin0.AccountActorCodeID || code == builtin2.AccountActorCodeID {
 		return methodStr(builtin2.MethodsAccount, methodNum)
 	}
 
-	if code == builtin2.InitActorCodeID {
+	if code == builtin0.InitActorCodeID || code == builtin2.InitActorCodeID {
 		return methodStr(builtin2.MethodsInit, methodNum)
 	}
 
-	if code == builtin2.CronActorCodeID {
+	if code == builtin0.CronActorCodeID || code == builtin2.CronActorCodeID {
 		return methodStr(builtin2.MethodsCron, methodNum)
 	}
 
-	if code == builtin2.RewardActorCodeID {
+	if code == builtin0.RewardActorCodeID || code == builtin2.RewardActorCodeID {
 		return methodStr(builtin2.MethodsReward, methodNum)
 	}
 
-	if code == builtin2.MultisigActorCodeID {
+	if code == builtin0.MultisigActorCodeID || code == builtin2.MultisigActorCodeID {
 		return methodStr(builtin2.MethodsMultisig, methodNum)
 	}
 
-	if code == builtin2.PaymentChannelActorCodeID {
+	if code == builtin0.PaymentChannelActorCodeID || code == builtin2.PaymentChannelActorCodeID {
 		return methodStr(builtin2.MethodsPaych, methodNum)
 	}
 
-	if code == builtin2.StorageMarketActorCodeID {
+	if code == builtin0.StorageMarketActorCodeID || code == builtin2.StorageMarketActorCodeID {
 		return methodStr(builtin2.MethodsMarket, methodNum)
 	}
 
-	if code == builtin2.StoragePowerActorCodeID {
+	if code == builtin0.StoragePowerActorCodeID || code == builtin2.StoragePowerActorCodeID {
 		return methodStr(builtin2.MethodsPower, methodNum)
 	}
 
-	if code == builtin2.StorageMinerActorCodeID {
+	if code == builtin0.StorageMinerActorCodeID || code == builtin2.StorageMinerActorCodeID {
 		return methodStr(builtin2.MethodsMiner, methodNum)
 	}
 
-	if code == builtin2.VerifiedRegistryActorCodeID {
+	if code == builtin0.VerifiedRegistryActorCodeID || code == builtin2.VerifiedRegistryActorCodeID {
 		return methodStr(builtin2.MethodsVerifiedRegistry, methodNum)
 	}
 
